1604d9dd48a1d07e3d3dc0611b40dddc7bb2940d
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-04-14 00:17:46 +0300

    add save theme to local storage and load theme from local storage on page refresh functionality, change setMode name to toggleMode, fix text-box color

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
 
-// Set Dark / Light Mode Styles
-function setMode(theme) {
+// Toggle Dark / Light Mode Styles
+function toggleMode(theme) {
   const lightMode = theme === 'light';
   const light = 'rgb(255 255 255 / 50%)';
   const dark = 'rgba(0 0 0 / 50%)';
@@ -28,13 +28,26 @@ function setMode(theme) {
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    setMode('dark');
+    localStorage.setItem('theme', 'dark');
+    toggleMode('dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    setMode('light');
+    localStorage.setItem('theme', 'light');
+    toggleMode('light');
   }
 }
 
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    toggleMode('dark');
+  }
+}
